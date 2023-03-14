0b96690f51baa7e1561ad7a5587741abfead22ae
Andrii Valenko <andreyvalenko@gmail.com>
2023-03-14 17:26:47 +0200

    s

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import user from './user.json';
+import Profile from 'components/Profile';
+
+export default function App() {
+  return (
+    <Profile
+      username={user.username}
+      tag={user.tag}
+      location={user.location}
+      avatar={user.avatar}
+      stats={user.stats}
+    />
+  );
+}
