37c347c1c2dcf0efe11d76b845d3df815be7f221
Andrii Valenko <andreyvalenko@gmail.com>
2023-03-15 09:14:23 +0200

    s

---
+++ src/components/statistics/Statistics.js
@@ -0,0 +1,26 @@
+import PropTypes from 'prop-types';
+
+export default function Statistics({ title = '', stats }) {
+  return (
+    <section className="statistics">
+      {title && <h2 className="title">{title}</h2>}
+      <ul className="stat-list">
+        {stats.map(stat => (
+          <li className="item" key={stat.id}>
+            <span className="label">{stat.label}</span>
+            <span className="percentage">{stat.percentage}%</span>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+}
+
+Statistics.propTypes = {
+  title: PropTypes.string,
+  stat: PropTypes.shape({
+    id: PropTypes.string,
+    label: PropTypes.string,
+    percentage: PropTypes.number,
+  }),
+};
